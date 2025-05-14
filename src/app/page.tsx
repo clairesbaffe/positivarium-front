dd977a7938a6506765d9b85f5813601af7a15a93
Claire Sbaffe <claire.sbaffe@efrei.net>
2025-05-14 11:46:32 +0200

    FEAT fetch current user in home page
    Simply use credentials include in requests, no need to manipulate cookie.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,22 @@ import {
   UserRoundPen,
 } from "lucide-react";
 import ArticlesPage from "@/components/articles/ArticlesPage";
+import { useEffect, useState } from "react";
+import { getCurrentUser } from "@/lib/data";
 
 export default function Home() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const userData = await getCurrentUser();
+      console.log("🚀 ~ fetchUser ~ userData:", userData)
+      setUser(userData);
+    };
+
+    fetchUser();
+  }, []);
+
   return (
     <main className="flex flex-col my-8 md:m-24 gap-32">
       <section className="flex flex-col-reverse md:flex-row items-center gap-8 md:gap-20">
