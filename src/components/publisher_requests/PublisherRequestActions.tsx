5dad3c869f23c9ec30cfe2d72d8d35111c51feea
Claire Sbaffe <claire.sbaffe@efrei.net>
2025-06-07 13:43:48 +0200

    FIX publisher requests table responsive
    Added DialogAlert to see details and actions

--- src/components/publisher_requests/PublisherRequestActions.tsx
+++ src/components/publisher_requests/PublisherRequestActions.tsx
@@ -50,115 +50,102 @@ export default function PublisherRequestActions({ id }: { id: number }) {
   };
 
   return (
-    <Popover>
-      <PopoverTrigger>
-        <Ellipsis className="cursor-pointer" />
-      </PopoverTrigger>
-      <PopoverContent className="bg-background">
-        <div className="flex flex-col gap-4">
-          {user?.roles.includes("ROLE_USER") && (
-            <AlertDialog>
-              <AlertDialogTrigger className="flex items-center justify-center gap-2 py-2.5 px-4 h-min rounded-md font-semibold bg-background-danger text-foreground whitespace-nowrap cursor-pointer">
-                Annuler la demande
-              </AlertDialogTrigger>
-              <AlertDialogContent>
-                <AlertDialogHeader>
-                  <AlertDialogTitle>
-                    Êtes-vous vraiment sûr(e) ?
-                  </AlertDialogTitle>
-                  <AlertDialogDescription>
-                    Cette demande sera annulée et ne pourra plus être acceptée.
-                  </AlertDialogDescription>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                  <AlertDialogCancel className="cursor-pointer">
-                    Annuler
-                  </AlertDialogCancel>
-                  <AlertDialogAction
-                    className="cursor-pointer"
-                    onClick={() => handleCancelClick()}
-                  >
-                    Continuer
-                  </AlertDialogAction>
-                </AlertDialogFooter>
-              </AlertDialogContent>
-            </AlertDialog>
-          )}
+    <div className="flex flex-wrap gap-4">
+      {user?.roles.includes("ROLE_USER") && (
+        <AlertDialog>
+          <AlertDialogTrigger className="flex items-center justify-center gap-2 py-2.5 px-4 h-min rounded-md font-semibold bg-background-danger text-foreground whitespace-nowrap cursor-pointer">
+            Annuler la demande
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Êtes-vous vraiment sûr(e) ?</AlertDialogTitle>
+              <AlertDialogDescription>
+                Cette demande sera annulée et ne pourra plus être acceptée.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel className="cursor-pointer">
+                Annuler
+              </AlertDialogCancel>
+              <AlertDialogAction
+                className="cursor-pointer"
+                onClick={() => handleCancelClick()}
+              >
+                Continuer
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      )}
 
-          {user?.roles.includes("ROLE_ADMIN") &&
-            !user.roles.includes("ROLE_BAN") && (
-              <Button
-                title="Marquer en révision"
-                background="bg-colored-background"
-                textColor=""
-                icon={null}
-                onClick={() => handleAdminClick("UNDER_REVIEW")}
-              />
-            )}
+      {user?.roles.includes("ROLE_ADMIN") &&
+        !user.roles.includes("ROLE_BAN") && (
+          <Button
+            title="Marquer en révision"
+            background="bg-colored-background"
+            textColor=""
+            icon={null}
+            onClick={() => handleAdminClick("UNDER_REVIEW")}
+          />
+        )}
 
-          {user?.roles.includes("ROLE_ADMIN") &&
-            !user.roles.includes("ROLE_BAN") && (
-              <AlertDialog>
-                <AlertDialogTrigger className="flex items-center justify-center gap-2 py-2.5 px-4 h-min rounded-md font-semibold bg-background-success text-foreground whitespace-nowrap cursor-pointer">
-                  Accepter
-                </AlertDialogTrigger>
-                <AlertDialogContent>
-                  <AlertDialogHeader>
-                    <AlertDialogTitle>
-                      Êtes-vous vraiment sûr(e) ?
-                    </AlertDialogTitle>
-                    <AlertDialogDescription>
-                      Cet utilisateur deviendra rédacteur. Cette action ne peut
-                      pas être annulée.
-                    </AlertDialogDescription>
-                  </AlertDialogHeader>
-                  <AlertDialogFooter>
-                    <AlertDialogCancel className="cursor-pointer">
-                      Annuler
-                    </AlertDialogCancel>
-                    <AlertDialogAction
-                      className="cursor-pointer"
-                      onClick={() => handleAdminClick("APPROVED")}
-                    >
-                      Continuer
-                    </AlertDialogAction>
-                  </AlertDialogFooter>
-                </AlertDialogContent>
-              </AlertDialog>
-            )}
+      {user?.roles.includes("ROLE_ADMIN") &&
+        !user.roles.includes("ROLE_BAN") && (
+          <AlertDialog>
+            <AlertDialogTrigger className="flex items-center justify-center gap-2 py-2.5 px-4 h-min rounded-md font-semibold bg-background-success text-foreground whitespace-nowrap cursor-pointer">
+              Accepter
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Êtes-vous vraiment sûr(e) ?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  Cet utilisateur deviendra rédacteur. Cette action ne peut pas
+                  être annulée.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel className="cursor-pointer">
+                  Annuler
+                </AlertDialogCancel>
+                <AlertDialogAction
+                  className="cursor-pointer"
+                  onClick={() => handleAdminClick("APPROVED")}
+                >
+                  Continuer
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+        )}
 
-          {user?.roles.includes("ROLE_ADMIN") &&
-            !user.roles.includes("ROLE_BAN") && (
-              <AlertDialog>
-                <AlertDialogTrigger className="flex items-center justify-center gap-2 py-2.5 px-4 h-min rounded-md font-semibold bg-background-danger text-foreground whitespace-nowrap cursor-pointer">
-                  Refuser
-                </AlertDialogTrigger>
-                <AlertDialogContent>
-                  <AlertDialogHeader>
-                    <AlertDialogTitle>
-                      Êtes-vous vraiment sûr(e) ?
-                    </AlertDialogTitle>
-                    <AlertDialogDescription>
-                      Cette demande sera refusée. L'utilisateur en sera informé
-                      et pourra en refaire une nouvelle.
-                    </AlertDialogDescription>
-                  </AlertDialogHeader>
-                  <AlertDialogFooter>
-                    <AlertDialogCancel className="cursor-pointer">
-                      Annuler
-                    </AlertDialogCancel>
-                    <AlertDialogAction
-                      className="cursor-pointer"
-                      onClick={() => handleAdminClick("REJECTED")}
-                    >
-                      Continuer
-                    </AlertDialogAction>
-                  </AlertDialogFooter>
-                </AlertDialogContent>
-              </AlertDialog>
-            )}
-        </div>
-      </PopoverContent>
-    </Popover>
+      {user?.roles.includes("ROLE_ADMIN") &&
+        !user.roles.includes("ROLE_BAN") && (
+          <AlertDialog>
+            <AlertDialogTrigger className="flex items-center justify-center gap-2 py-2.5 px-4 h-min rounded-md font-semibold bg-background-danger text-foreground whitespace-nowrap cursor-pointer">
+              Refuser
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Êtes-vous vraiment sûr(e) ?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  Cette demande sera refusée. L'utilisateur en sera informé et
+                  pourra en refaire une nouvelle.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel className="cursor-pointer">
+                  Annuler
+                </AlertDialogCancel>
+                <AlertDialogAction
+                  className="cursor-pointer"
+                  onClick={() => handleAdminClick("REJECTED")}
+                >
+                  Continuer
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+        )}
+    </div>
   );
 }
--- src/components/publisher_requests/PublisherRequestsList.tsx
+++ src/components/publisher_requests/PublisherRequestsList.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { PublisherRequest } from "@/lib/definitions";
 import { publisherRequestStatus } from "@/lib/utils";
 
@@ -10,8 +12,21 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import PaginationControls from "@/components/Pagination";
 import PublisherRequestActions from "@/components/publisher_requests/PublisherRequestActions";
+import { useState } from "react";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import { Eye } from "lucide-react";
 
 export default function PublisherRequestsList({
   currentPage,
@@ -35,16 +50,18 @@ export default function PublisherRequestsList({
             {type === "admin" && (
               <TableHead className="w-[100px]">Utilisateur</TableHead>
             )}
-            <TableHead className="w-[500px]">Motivation</TableHead>
+            <TableHead className="min-w-[200px]">Motivation</TableHead>
             <TableHead className="w-[100px]">Date de demande</TableHead>
             <TableHead className="w-[100px]">Dernière modification</TableHead>
-            <TableHead className="w-[50px]"></TableHead>
+            <TableHead className="w-[25px]"></TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           {requests.map((request) => {
             const status: { name: string; bg: string; color: string } =
               publisherRequestStatus(request.status);
+
+            const [isDialogOpen, setIsDialogOpen] = useState(false);
 
             return (
               <TableRow key={request.id}>
@@ -60,8 +77,8 @@ export default function PublisherRequestsList({
                     <p>{request.username}</p>
                   </TableCell>
                 )}
-                <TableCell className="whitespace-normal">
-                  <p>{request.motivation}</p>
+                <TableCell>
+                  <p className="line-clamp-6 whitespace-pre-line">{request.motivation}</p>
                 </TableCell>
                 <TableCell>
                   {new Date(request.createdAt).toLocaleString("fr-FR")}
@@ -70,11 +87,81 @@ export default function PublisherRequestsList({
                   {new Date(request.updatedAt).toLocaleString("fr-FR")}
                 </TableCell>
                 <TableCell>
-                  {request.status !== "APPROVED" &&
-                    request.status !== "REJECTED" &&
-                    request.status !== "CANCELLED" && (
-                      <PublisherRequestActions id={request.id} />
-                    )}
+                  <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+                    <DialogTrigger
+                      className={`bg-opacity-100 py-2.5 px-4 h-min rounded-md font-semibold text-foreground cursor-pointer`}
+                      onClick={() => setIsDialogOpen(true)}
+                    >
+                      <Eye />
+                    </DialogTrigger>
+                    <DialogContent className="sm:max-w-[800px] max-h-[90vh] overflow-y-scroll">
+                      <DialogHeader>
+                        <DialogTitle className="flex gap-4 items-center">
+                          Demande rédacteur{" "}
+                          <p
+                            className={`${status.bg} ${status.color} py-1 px-2 rounded-3xl w-min whitespace-nowrap`}
+                          >
+                            {status.name}
+                          </p>
+                        </DialogTitle>
+                      </DialogHeader>
+
+                      {request.status !== "APPROVED" &&
+                        request.status !== "REJECTED" &&
+                        request.status !== "CANCELLED" && (
+                          <PublisherRequestActions id={request.id} />
+                        )}
+
+                      <div className="text-foreground-muted">
+                        <p>
+                          Envoyée le{" "}
+                          {new Date(request.createdAt).toLocaleString("fr-FR", {
+                            day: "2-digit",
+                            month: "long",
+                            year: "numeric",
+                            hour: "2-digit",
+                            minute: "2-digit",
+                          })}
+                        </p>
+                        {request.createdAt !== request.updatedAt && (
+                          <p>
+                            Modifiée le{" "}
+                            {new Date(request.updatedAt).toLocaleString(
+                              "fr-FR",
+                              {
+                                day: "2-digit",
+                                month: "long",
+                                year: "numeric",
+                                hour: "2-digit",
+                                minute: "2-digit",
+                              }
+                            )}
+                          </p>
+                        )}
+                      </div>
+
+                      {type === "admin" && (
+                        <p>
+                          Envoyée par{" "}
+                          <Link href={`/admin/users/${request.username}`}>
+                            {request.username}
+                          </Link>
+                        </p>
+                      )}
+                      <p className="whitespace-pre-line">{request.motivation}</p>
+
+                      <DialogFooter>
+                        <Button
+                          type="submit"
+                          variant="outline"
+                          className="cursor-pointer"
+                          onClick={() => setIsDialogOpen(false)}
+                        >
+                          Fermer
+                        </Button>
+                      </DialogFooter>
+                    </DialogContent>
+                  </Dialog>
                 </TableCell>
               </TableRow>
             );
