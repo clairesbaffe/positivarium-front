19953c86fc4491873d613a1352b83e65c2089c85
Claire Sbaffe <claire.sbaffe@efrei.net>
2025-06-16 10:38:26 +0200

    FIX auto deploy on Vercel

--- src/tests/sum.test.ts
+++ src/tests/sum.test.ts
@@ -2,7 +2,7 @@ import sum from './sum';
 import {describe, expect, test} from '@jest/globals';
 
 describe('sum module', () => {
-  test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+  test('adds 1 + 3 to equal 4', () => {
+    expect(sum(1, 3)).toBe(4);
   });
 });
