aadf83188e1e54807ae0c403dc8dee04908591e7
Claire Sbaffe <claire.sbaffe@efrei.net>
2025-06-16 11:01:59 +0200

    FIX Vercel auto deploy on workflow success

--- src/tests/sum.test.ts
+++ src/tests/sum.test.ts
@@ -1,8 +1,8 @@
-import sum from './sum';
-import {describe, expect, test} from '@jest/globals';
+import sum from "./sum";
+import { describe, expect, test } from "@jest/globals";
 
-describe('sum module', () => {
-  test('adds 1 + 3 to equal 4', () => {
-    expect(sum(1, 3)).toBe(4);
+describe("sum module", () => {
+  test("adds 1 + 2 to equal 3", () => {
+    expect(sum(1, 2)).toBe(3);
   });
 });
